e8fdad2743e7688ee2724d96958630c9759d3099
Yannis Saliniere <yannis.saliniere@gmail.com>
2021-08-31 23:19:32 +0200

    version 0.0.13 modif factories.py serializers.py test_serializers.py

--- crm/crm_api/serializers.py
+++ crm/crm_api/serializers.py
@@ -1,42 +1,46 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from crm_api.models import SalesContact, SupportContact, StaffContact, Client, Contract, Event
+
+from crm_api.models import (Client, Contract, Event, SalesContact,
+                            StaffContact, SupportContact, User)
 
 
 class ClientSerializer(serializers.ModelSerializer):
     """
     Client serializer
     """
-    sales_contact_id = serializers.ReadOnlyField(source='sales_contact.id')
+    sales_contact_id = serializers.ReadOnlyField(source='sales_contact.sales_contact_id')
 
     class Meta:
         model = Client
-        fields = ['first_name', 'last_name', 'email', 'phone', 'mobile', 'sales_contact_id']
+        fields = ['client_id', 'first_name', 'last_name',
+                  'email', 'phone', 'mobile', 'sales_contact_id']
 
 
 class ContractSerializer(serializers.ModelSerializer):
     """
     Contract serializer
     """
-    sales_contact_id = serializers.ReadOnlyField(source='sales_contact.id')
-    client_id = serializers.ReadOnlyField(source='client.id')
+    sales_contact_id = serializers.ReadOnlyField(source='sales_contact.sales_contact_id')
+    client_id = serializers.ReadOnlyField(source='client.client_id')
 
     class Meta:
         model = Contract
-        fields = ['sales_contact_id', 'client_id', 'status', 'amount', 'payment_due']
+        fields = ['contract_id', 'sales_contact_id', 'client_id',
+                  'status', 'amount', 'payment_due']
 
 
 class EventSerializer(serializers.ModelSerializer):
     """
     Event serializer
     """
-    support_contact_id = serializers.ReadOnlyField(source='support_contact.id')
-    client_id = serializers.ReadOnlyField(source='client.id')
-    event_status_id = serializers.ReadOnlyField(source='event_status.id')
+    support_contact_id = serializers.ReadOnlyField(source='support_contact.support_contact_id')
+    client_id = serializers.ReadOnlyField(source='client.client_id')
+    event_status_id = serializers.ReadOnlyField(source='event_status.event_status_id')
 
     class Meta:
         model = Event
-        fields = ['client_id', 'support_contact_id', 'event_status_id', 'attendees', 'event_date', 'notes']
+        fields = ['event_id', 'client_id', 'support_contact_id',
+                  'event_status_id', 'attendees', 'event_date', 'notes']
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -45,34 +49,34 @@ class UserSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['user_id', 'username', 'password']
 
 
 class SalesContactSerializer(serializers.ModelSerializer):
-    user_id = serializers.ReadOnlyField(source='user.id')
+    user_id = serializers.ReadOnlyField(source='user.user_id')
     username = serializers.ReadOnlyField(source='user.username')
     password = serializers.ReadOnlyField(source='user.password')
 
     class Meta:
         model = SalesContact
-        fields = ['user_id', 'username', 'password']
+        fields = ['sales_contact_id', 'user_id', 'username', 'password']
 
 
 class SupportContactSerializer(serializers.ModelSerializer):
-    user_id = serializers.ReadOnlyField(source='user.id')
+    user_id = serializers.ReadOnlyField(source='user.user_id')
     username = serializers.ReadOnlyField(source='user.username')
     password = serializers.ReadOnlyField(source='user.password')
 
     class Meta:
         model = SupportContact
-        fields = ['user_id', 'username', 'password']
+        fields = ['support_contact_id', 'user_id', 'username', 'password']
 
 
 class StaffContactSerializer(serializers.ModelSerializer):
-    user_id = serializers.ReadOnlyField(source='user.id')
+    user_id = serializers.ReadOnlyField(source='user.user_id')
     username = serializers.ReadOnlyField(source='user.username')
     password = serializers.ReadOnlyField(source='user.password')
 
     class Meta:
         model = StaffContact
-        fields = ['user_id', 'username', 'password']
+        fields = ['staff_contact_id', 'user_id', 'username', 'password']
