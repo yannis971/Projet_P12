2cded58199c7e9de88f00d624374190d8b06fa6f
Yannis Saliniere <yannis.saliniere@gmail.com>
2021-09-26 18:23:16 +0200

    version 0.2.2 suppression sql

--- crm/auth/sql/group.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO auth_group (id, name)
-    VALUES
-        (1, 'STAFF'),
-        (2, 'SALES'),
-        (3, 'SUPPORT');
--- crm/crm_api/sql/eventstatus.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO crm_api_eventstatus (id, status)
-    VALUES
-        (1, 'C'),
-        (2, 'E'),
-        (3, 'P');
